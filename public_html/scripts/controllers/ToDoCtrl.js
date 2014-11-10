be024800cc9a3c9e409b805e374b38160a432097
Daniel Meiers <daniel.meiers@cismet.de>
2014-11-10 09:38:40 +0100

    added controller

---
+++ public_html/scripts/controllers/ToDoCtrl.js
@@ -0,0 +1,14 @@
+angular.module(
+    'todoApp',
+    []
+    ).controller(
+    'TodoCtrl',
+    [
+        '$scope',
+        function ($scope) {
+            $scope.todos = ['Learn Bower', 'Learn Angular Basic Concepts',
+                'Learn Angular Directives', 'Learn Angular Routing', 'Learn Grunt',
+                'Implement Html 5 SIP', 'Implement Html 5 Navigator'];
+        }
+    ]
+    )
