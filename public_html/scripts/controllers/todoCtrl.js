97e70a7e341ea8bf98c0c5e53b4bd324cb0ea5cf
Daniel Meiers <daniel.meiers@cismet.de>
2014-11-11 09:27:51 +0100

    fetching data from cids rest server

--- public_html/scripts/controllers/todoCtrl.js
+++ public_html/scripts/controllers/todoCtrl.js
@@ -8,14 +8,53 @@ angular.module(
         '$resource',
         function ($scope, shortenFilter, $resource) {
 
-            var todoResource;
-            
+            var todoResource, processQueryResult, transformRequest;
+
             $scope.name = 'Mr. X';
-            $scope.todos=[];
-    
-            todoResource = $resource('resources/todos/:id.json');
-            
-            $scope.todos = todoResource.query();
+            $scope.todos = [];
+
+            processQueryResult = function (todos) {
+                var todoArr = JSON.parse(todos).$collection;
+                return todoArr;
+            };
+
+            transformRequest= function (data) {
+                var transformedData;
+                
+                transformedData = JSON.stringify(data, function (k, v) {
+                    // we have to take care of angular properties by ourselves
+                    if (k.substring(0, 1) === '$' && !(k === '$self' || k === '$ref')) {
+                        return undefined;
+                    }
+
+                    return v;
+                });
+                return transformedData;
+            };
+
+            todoResource = $resource('http://localhost:8890/CIDS.todos/:todoId', {
+                todoId:'@id',
+                deduplicate: false,
+                level: '1',
+                omitNullValues: 'false'
+            }, {
+                'query': {
+                    method: 'GET',
+                    isArray: true,
+                    transformResponse: processQueryResult
+                },
+                'update': {
+                    method: 'PUT',
+                    transformRequest:transformRequest
+                }
+            });
+
+            todoResource.query(function (data) {
+                $scope.todos = data;
+//                $scope.firstTodo = $scope.todos[0];
+//                $scope.firstTodo.done = false;
+//                $scope.firstTodo.$update();
+            });
 
             $scope.addTodo = function () {
                 $scope.todos.push({
