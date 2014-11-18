bacd9c985c5393c00d15150c26fe6edd08635cbc
Daniel Meiers <daniel.meiers@cismet.de>
2014-11-18 16:01:18 +0100

    directive  and

--- public_html/scripts/directives/todoList.js
+++ public_html/scripts/directives/todoList.js
@@ -15,38 +15,4 @@ angular.module(
             return directiveDefinition;
         }
     ]
-).directive(
-    'myCurrentTime',
-    [
-        '$interval',
-        'dateFilter',
-        function ($interval, dateFilter) {
-
-            function link (scope, element, attrs) {
-                var format, timeoutId, updateTime;
-
-                updateTime = function updateTime () {
-                    element.text(dateFilter(new Date(), format));
-                }
-
-                scope.$watch(attrs.myCurrentTime, function (value) {
-                    format = value;
-                    updateTime();
-                });
-
-                element.on('$destroy', function () {
-                    $interval.cancel(timeoutId);
-                });
-
-                // start the UI update process; save the timeoutId for canceling
-                timeoutId = $interval(function () {
-                    updateTime(); // update DOM
-                }, 1000);
-            }
-
-            return {
-                link: link
-            };
-        }
-    ]
-);
+    );
